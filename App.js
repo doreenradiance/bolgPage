9c1d3654eb9fb73bf6c8c914e50e3bdec63542bd
Doreen Mensah <doreenradiance@gmail.com>
2020-12-07 11:57:21 +0000

    Changed the locatin of the screens and the components folder

--- App.js
+++ App.js
@@ -1,21 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import BlogsScreen from './src/screens/BlogsScreen';
+import SingleBlog  from './src/components/SingleBlog';
+import  BlogsDetail from './src/screens/BlogsDetail';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {/* <BlogsScreen/> */}
+      {/* <SingleBlog/> */}
+      <BlogsDetail/>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    
   },
 });
