d836b0067c9dbced05dd2b0f7d194353f590a236
Doreen Mensah <doreenradiance@gmail.com>
2020-12-10 16:20:28 +0000

    Adding and Changing Styles

--- src/screens/BlogsDetail.js
+++ src/screens/BlogsDetail.js
@@ -1,35 +1,47 @@
 import React from 'react'
-import { StyleSheet, Text, View, Image } from 'react-native'
+import {ScrollView, StyleSheet, Text, View, Image, } from 'react-native'
 import { AntDesign, Feather, Entypo, SimpleLineIcons } from '@expo/vector-icons';
 
 
 
 export default function BlogsDetail() {
     return (
-        <View style={styles.mainContainer}>
+        <ScrollView style={styles.mainContainer}>
             <View style={styles.cotainer}>
                 <View style={styles.heading}>
                     <AntDesign name="left" style={styles.left} size={30} />
                     <Text style={styles.headingText}>Beauty</Text>
-                    <Feather name="headphones" style={styles.headPhone} size={20} />
-                    <Entypo name="heart-outlined" style={styles.heart} size={20} />
-                    <SimpleLineIcons name="share" style={styles.share} size={20} color="black" />
+                    <View style={styles.icons}>
+                     <Feather name="headphones" style={styles.headPhone} size={25} />
+                     <Entypo name="heart-outlined" style={styles.heart} size={25} />
+                     <SimpleLineIcons name="share" style={styles.share} size={25} color="black" />
+                    </View>
                 </View>
 
-                <View style={styles.firstImage}>
-                    <Image source={require('../../assets/flower1.jpg') }/>
+                <View >
+                    <Image source={require('../../assets/flower1.jpg')} style={styles.firstImage}/>
                 </View>
 
-                <Text style={styles.boldText}>
+               <View style={styles.boldText}> 
+                <Text style={styles.text1}>
                     Focus On Learning and Creating
-                Rather Than Entertainment
-                and Distraction
-                 </Text>
+                    </Text>
+                    <Text style={styles.text2}>
+                    Rather Than Entertainment and 
+                    </Text>
+                <Text style={styles.text3}>
+                    Distraction
+                    </Text>
+                </View>
+                 
 
                 <View style={styles.profile}>
                     <Image source={require('../../assets/DP.jpg')} style={styles.secondImage} />
-                    <Text> Jonhy Vino
-                    . 4 min read</Text>
+                    <View style={styles.profileText}>
+                    <Text> Jonhy Vino</Text>
+                    <View style={styles.indicator}></View>
+                    <Text style={styles.time}>4 min read</Text>
+                    </View>
                 </View>
 
                 <Text style={styles.text}>Contrary to popular belief,
@@ -49,17 +61,18 @@ export default function BlogsDetail() {
                    </Text>
 
             </View>
-        </View>
+        </ScrollView>
     )
 }
 
 const styles = StyleSheet.create({
     mainContainer:{
         flex: 1,
-        marginTop:80,
+        marginTop:40,
         marginLeft:15,
         // justifyContent: "space-around",
     },
+
     container: {
         flex: 1,
         marginTop: 30,
@@ -71,46 +84,102 @@ const styles = StyleSheet.create({
  heading:{
      flexDirection:"row",
      alignContent:"space-between"
-
  },
+
  headingText:{
      fontWeight:"bold",
      fontSize:25,
-     marginRight:80,
-    
+     marginRight:80
  },
+
+ icons:{
+     marginLeft:20,
+     flexDirection:"row"
+},
+
  left:{
-// marginTop:3
+marginTop:3
  },
+
  headPhone:{
      marginRight:20,
-     marginTop:5
+     marginTop:6
  },
+
 heart:{
     marginRight:20,
-    marginTop:5
+    marginTop:6
 },
+
 share:{
     marginRight:20,
-    marginTop:5
+    marginTop:6
 },
+
  firstImage:{
-    height:10,
-    width:10
+     alignSelf:"center",
+    height:250,
+    width:250,
+    marginTop:25
+    // backgroundColor:'red'
  },
+
  secondImage:{
      height:30,
      width:30,
-     borderRadius:15
+     borderRadius:15,
  },
+
+    indicator:{
+        marginVertical:10,
+        marginLeft:15,
+        marginRight:5,
+        backgroundColor:"#444952",
+        height:5,
+        width:5,
+        borderRadius:2.5
+ },
+ time:{
+     color:"#444952",
+     marginTop:3
+ },
+
  boldText:{
-     fontWeight:"bold",
-     fontSize:15
+     marginLeft:15,
+     marginTop:10
  },
+
+ text1:{
+     fontSize:19,
+     fontWeight:"bold",
+},
+
+text2:{
+    fontSize:19,
+    fontWeight:"bold",
+},
+
+text3:{
+    fontSize:19,
+    fontWeight:"bold",
+},
+
  profile:{
      flexDirection:"row",
+     marginLeft:15,
+     marginTop:15
+ },
+
+ profileText:{
+     flexDirection:"row",
+     marginTop:5
  },
 text:{
-    fontSize:18
-}
+    fontSize:15,
+    marginTop:15,
+    marginLeft:10,
+    marginRight:10
+},
+
+ 
 })
